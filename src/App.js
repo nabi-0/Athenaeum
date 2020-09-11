62adad6420d93de0cd3f2d7395acea227c073ecc
jarrellrn <jarrellrn@gmail.com>
2020-09-10 21:20:33 -0500

    initialized main and nav components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Account from "./components/Account/Account";
+// import Account from "./components/Account/Account";
 import Main from "./components/Main/Main";
-import Search from "./components/Search/Search";
-import Settings from "./components/Settings/Settings";
+// import Search from "./components/Search/Search";
+// import Settings from "./components/Settings/Settings";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Route path="/" exact>
-          <Main />
-        </Route>
-        <Route path="/account" exact>
-          <Account />
-        </Route>
-        <Route path="/search" exact>
-          <Search />
-        </Route>
-        <Route path="/settings" exact>
-          <Settings />
-        </Route>
-      </Router>
-    </div>
+    <Router>
+      <Route path="/" exact>
+        <Main />
+      </Route>
+      {/* <Route path="/account" exact>
+        <Account />
+      </Route>
+      <Route path="/search" exact>
+        <Search />
+      </Route>
+      <Route path="/settings" exact>
+        <Settings />
+      </Route> */}
+    </Router>
   );
 }
 
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./style.css";
+import { Link } from "react-router-dom";
+
+function Main() {
+  return (
+    <div className="navBody">
+      <h2>Athaenaeum | nav bar</h2>
+      <div id="container1">
+        <ul className="navbarList">
+          <li>test1</li>
+          <li>test1</li>
+          <li>test1</li>
+          <li>test1</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
