f8c6157cae1cde198c57530185436b99044894a3
jarrellrn <jarrellrn@gmail.com>
2020-09-22 11:22:10 -0500

    added api routes

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
@@ -5,7 +7,7 @@ const passport = require("passport");
 const passportLocal = require("passport-local");
 const cookieParser = require("cookie-parser");
 const bcrypt = require("bcryptjs");
-const expressSession = require("express-session");
+const session = require("express-session");
 const bodyParser = require("body-parser");
 const PORT = 3000;
 
@@ -14,7 +16,30 @@ const app = express();
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "http://localhost:4000", // <-- location of the react ap we're connecting to
+    credentials: true,
+  })
+);
+app.use(
+  session({
+    secret: process.env.sessionsecret,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(cookieParser(process.env.sessionsecret));
+// Routes
+app.post("/login", (req, res) => {
+  console.log(req.body);
+});
+app.post("/register", (req, res) => {
+  console.log(req.body);
+});
+app.get("/user", (req, res) => {});
 
+// Start server
 app.listen(PORT, () => {
   console.log(`Server has started on port ${PORT}.`);
 });
