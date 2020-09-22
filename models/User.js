5f117f52d37380e04e0ddade68e251416f468376
jarrellrn <jarrellrn@gmail.com>
2020-09-22 14:25:43 -0500

    more work on User model

--- models/User.js
+++ models/User.js
@@ -3,5 +3,18 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   username: {
     type: String,
+    required: true,
+    min: 6,
+    max: 15,
   },
+  password: {
+    type: String,
+    required: true,
+  },
+  //   role: {
+  //     type: String,
+  //     enum: ["user", "admin"],
+  //     required: true,
+  //   },
+  //   todos: [{ type: mongoose.Schema.Types.ObjectId, ref: "Todo" }],
 });
