1c688fd2511e85f3d8fb537895ef6014db1b54de
jarrellrn <jarrellrn@gmail.com>
2020-09-22 14:31:28 -0500

    more User.js work

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   username: {
@@ -18,3 +19,24 @@ const UserSchema = new mongoose.Schema({
   //   },
   //   todos: [{ type: mongoose.Schema.Types.ObjectId, ref: "Todo" }],
 });
+
+UserSchema.pre("save", function (next) {
+  if (!this.isModified("password")) return next();
+  bcrypt.hash(this.password, 10, (err, passwordHash) => {
+    if (err) return next(err);
+    this.password = passwordHash;
+    next();
+  });
+});
+
+UserSchema.methods.comparePassword = function (password, cb) {
+  bcrypt.compare(password, this.password, (err, isMatch) => {
+    if (err) return cb(err);
+    else {
+      if (!isMatch) return cb(null, isMatch);
+      return cb(null, this);
+    }
+  });
+};
+
+module.exports = mongoose.model("User", UserSchema);
